724ecb75a59581cd5e478fa88802db5d163e2c57
Yuriy Gatilin <modernptz@gmail.com>
2015-06-12 14:40:46 +0300

    Total results

--- Team5/crystal/calculator.cr
+++ Team5/crystal/calculator.cr
@@ -1,26 +1,32 @@
 class Calculate
   def initialize(file)
-    @dept = 1
-    @sum = @lines = 0
-    rasa = calculate(file)
-    puts "#{file}: lines #{@lines}, RaSa: #{rasa}"
+    @res = calculate(file)
+    puts "#{file}: lines #{res[0]}, RaSa: #{res[1]}"
   end
 
-  def calculate(file)
+  def res
+    @res
+  end
+
+  def calculate(file, dept = 1, sum = 0, lines = 0)
     File.open(file, "r") do |infile|
       while (line = infile.gets)
-        @lines += 1
+        lines += 1
         line.each_char do |sym|
-          @dept += 1 if(sym == '{')
-          @dept -= 1 if(sym == '}')
-          @sum += @dept if (sym == ';')
+          dept += 1 if(sym == '{')
+          dept -= 1 if(sym == '}')
+          sum += dept if (sym == ';')
         end
       end
-      @sum
+      res = [lines, sum] of Int32
     end
   end
 end
 
+lines = rasa = 0
 ARGV.each do |f|
-  Calculate.new(f)
+  c = Calculate.new(f)
+  res = c.res
+  lines += res[0]; rasa += res[1]
 end
+puts "Total: lines #{lines}, RaSa: #{rasa}"
