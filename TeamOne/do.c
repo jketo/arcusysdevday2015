d6facb8357bb2178f72fa2a1edd72c3c480b0ca3
Jarmo Pussinen <jarmo.pussinen@arcusys.fi>
2015-06-12 15:28:05 +0300

    C-version

---
+++ TeamOne/do.c
@@ -0,0 +1,52 @@
+#include <stdio.h> 
+#include <stdlib.h>
+#include <string.h>
+
+int timer=1;
+int lines=0;
+int rask=0;
+int allLines=0;
+
+void DoRasa(char *file) {
+    int timer=1;
+    int lines=0;
+    int rask=0;
+    int allLines=0;
+    char line[1024] = {0};
+    FILE *f = fopen(file, "r");
+    while ( fgets(line, 1024, f)) {
+        allLines++;
+        int i = 0;
+        while (i < strlen(line)) {
+            char c = line[i];
+            if (c == ';') {
+                lines++;
+            }
+            if (c == '{' ) {
+                rask += lines * timer;
+                timer++;
+                lines = 0;
+            }
+            if (c == '}') {
+                rask += lines * timer;
+                if (timer > 1) {
+                    timer--;
+                }
+                lines=0;
+            }
+            ++i;
+        }
+    }
+    rask += lines * timer;
+    fclose(f);  
+    printf("%s: lines %d, Rasa %d\n", file, allLines, rask);
+}
+
+int main(int argc, char**argv) {
+    int i = 1;
+    while (i < argc) {
+        DoRasa(argv[i]);
+        ++i;
+    }
+}
+
