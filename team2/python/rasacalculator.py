45e74d39fa99e954dd7e22b9882eb64c3552929a
Jere Malinen <jere.malinen@arcusys.fi>
2015-06-12 14:12:24 +0300

    Minify more

--- team2/python/rasacalculator.py
+++ team2/python/rasacalculator.py
@@ -1,35 +1,26 @@
 import argparse
-
-
-def calculateFileRasa(filePath):
-    rowCount = 0
-    multiplier = 1
-    rasa = 0
-
-    for line in open(filePath):
-        rowCount += 1
-        for char in line:
-            if '{' in char:
-                multiplier += 1
-            if ';' in char:
-                rasa += multiplier
-            if '}' in char:
-                multiplier -= 1
-    return (rowCount, rasa)
-
-
-def main(args):
-    totalRows = 0
-    totalRasa = 0
-    for filePath in args.argument:
-        rowCount, rasa = calculateFileRasa(filePath)
-        totalRows += rowCount
-        totalRasa += rasa
-        print '%s: lines %d, RaSa: %d' % (filePath, rowCount, rasa)
-    print 'total: lines %d, RaSa: %d' % (totalRows, totalRasa)
-
-
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument('argument', nargs='*')
-    main(parser.parse_args())
+G=open
+K=argparse.ArgumentParser
+def u(z):
+ r=0
+ b=1
+ I=0
+ for m in G(z):
+  r+=1
+  for k in m:
+   if '{' in k:b+=1
+   if ';' in k:I+=b
+   if '}' in k:b-=1
+ return(r,I)
+def B(args):
+ c=0
+ D=0
+ for z in args.argument:
+  r,I=u(z)
+  c+=r
+  D+=I
+  print '%s: lines %d, RaSa: %d'%(z,r,I)
+ print 'total: lines %d, RaSa: %d'%(c,D)
+C=K()
+C.add_argument('argument',nargs='*')
+B(C.parse_args())
