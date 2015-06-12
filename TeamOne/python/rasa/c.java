6907694616e0a80a58a1f9f98e6a833ee3f18b01
Ville Vähäkainu <ville.vahakainu@arcusys.fi>
2015-06-12 13:11:56 +0300

    Initial Team One python

---
+++ TeamOne/python/rasa/c.java
@@ -0,0 +1,11 @@
+void test()
+{ int a = 0;        
+  int b = 1;
+  if (a > b) {
+System.out.println(a);
+  } else {
+System.out.println(a);
+  }
+  System.out.println("Bye");
+}
+
---
+++ TeamOne/python/rasa/rasa.py
@@ -0,0 +1,26 @@
+__author__ = 'ville'
+import sys
+
+def count_files():
+    filenames = sys.argv[1:]
+    complexity = 0
+    for filename in filenames:
+        f = open(filename)
+        complexity += count_rasa(f)
+    print complexity
+
+def count_rasa(f):
+    multiplier = 1
+    complexity = 0
+    for line in f:
+        for char in line:
+            if char == "{":
+                multiplier += 1
+            elif char == "}":
+                multiplier -= 1
+            elif char == ";":
+                complexity += multiplier
+    return complexity
+
+if __name__ == "__main__":
+    count_files()
