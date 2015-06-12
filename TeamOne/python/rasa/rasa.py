56c61d4b813387ee8c410a8bee39d9173ca89fd4
Ville Vähäkainu <ville.vahakainu@arcusys.fi>
2015-06-12 14:26:09 +0300

    python fix prints

--- TeamOne/python/rasa/rasa.py
+++ TeamOne/python/rasa/rasa.py
@@ -3,24 +3,30 @@ import sys
 def count_files():
     filenames = sys.argv[1:]
     complexity = 0
+    lines = 0
     for filename in filenames:
-        f = open(filename)
-        complexity += count_rasa(f)
-        f.close()
-    print complexity
+        file_complexity, file_lines = count_rasa(filename)
+        complexity += file_complexity
+        lines += file_lines
 
-def count_rasa(f):
+    print "total: lines %s, RaSa %s" % (lines, complexity)
+
+def count_rasa(filename):
     multiplier = 1
     complexity = 0
-    for line in f:
-        for char in line:
-            if char == "{":
-                multiplier += 1
-            elif char == "}":
-                multiplier -= 1
-            elif char == ";":
-                complexity += multiplier
-    return complexity
+    lines = 0
+    with open(filename) as fh:
+        for line in fh:
+            for char in line:
+                if char == "{":
+                    multiplier += 1
+                elif char == "}":
+                    multiplier -= 1
+                elif char == ";":
+                    complexity += multiplier
+            lines += 1
+    print "%s: lines %s, RaSa %s" % (filename, lines, complexity)
+    return complexity, lines
 
 if __name__ == "__main__":
     count_files()
