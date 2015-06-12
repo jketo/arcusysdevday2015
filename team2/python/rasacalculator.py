61298a90119712f6f8768870f0f5ab02c21d35c2
Jere Malinen <jere.malinen@arcusys.fi>
2015-06-12 13:22:43 +0300

    Count lines for given files

--- team2/python/rasacalculator.py
+++ team2/python/rasacalculator.py
@@ -2,11 +2,28 @@
 import argparse, logging
 
 
+def calculateFileRasa(filePath):
+    rowCount = 0
+    for line in open(filePath):
+        rowCount += 1
+    return (rowCount, 0)
+
+
+def calculateRasa(filePaths):
+    totalRows = 0
+    totalRasa = 0
+    for filePath in filePaths:
+        rowCount, rasa = calculateFileRasa(filePath)
+        totalRows += rowCount
+        totalRasa += rasa
+        print '%s: lines %d, RaSa: TBD' % (filePath, rowCount)
+    print 'total: lines %d, RaSa: TBD' % (totalRows)
+
+
 def main(args, loglevel):
     logging.basicConfig(format="%(levelname)s: %(message)s", level=loglevel)
-
-    print "Hello there."
     logging.debug("Your Arguments: %s" % args.argument)
+    calculateRasa(args.argument)
 
 
 if __name__ == '__main__':
