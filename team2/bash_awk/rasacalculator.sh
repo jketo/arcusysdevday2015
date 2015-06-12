342cef6669a201a9ef6fc503487b8e28f243467a
Dmitry Afanasyev <dmitry.afanasyev@arcusys.fi>
2015-06-12 14:56:50 +0300

    Small fix

--- team2/bash_awk/rasacalculator.sh
+++ team2/bash_awk/rasacalculator.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-for f in $@;do echo "$f:" $(awk 'BEGIN{r=0;l=0;d=1;}/$/{l++;}/{/{d++;}/;/{r+=d;}/}/{d--;}END{print "lines",l,"RaSa",r;}' $f);done
+for f in $@;do echo "$f:" $(awk 'BEGIN{FS="";r=0;l=0;d=1;}/$/{l++;}/{/{d++;}/;/{r+=d;}/}/{d--;}END{print "lines",l,"RaSa",r;}' $f);done
 
