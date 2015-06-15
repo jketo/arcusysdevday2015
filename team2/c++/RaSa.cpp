29cbcd5499feaa66edc9c24e860ef096e6ada4e2
Matti Lupari <matti.lupari@arcusys.fi>
2015-06-15 13:01:53 +0300

    c++ impl forgotten

---
+++ team2/c++/RaSa.cpp
@@ -0,0 +1,46 @@
+//============================================================================
+// Name        : RaSa2.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <algorithm>
+
+void processFile(int& lines, int& complexity, std::string path) {
+	std::string line;
+	std::ifstream infile(path);
+	int depth= 1;
+	while (std::getline(infile, line))
+	{
+		for (int i = 0; i < line.length(); ++i) {
+			if (line[i] == '{') {
+				depth++;
+			}
+			if (line[i] == '}') {
+				depth--;
+			}
+			if (line[i] == ';') {
+				complexity += depth;
+			}
+		}
+		lines++;
+	}
+	std::cout << "file: " << path << " lines: " << lines << " RaSa: " << complexity << std::endl;
+}
+
+
+int main(int argc, char* argv[]) {
+	int tl = 0; int tc = 0;
+	for (int i = 1; i < argc; ++i) {
+		int lines = 0; int complexity = 0;
+		processFile(lines, complexity, argv[i]);
+		tl += lines;
+		tc += complexity;
+	}
+	std::cout << "total lines: " << tl << " RaSa: " << tc << std::endl;
+}
